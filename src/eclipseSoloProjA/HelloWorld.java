0b58e019fac9b8044b4c02b952a6b3e88226367d
andre <andre@164.76.16.217>
2025-02-03 12:33:12 -0500

    Programmer 2 did this

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String [] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("This is my second print  Statement.");
+		System.out.println("Programmer 2 did this.");
 	}
 }
