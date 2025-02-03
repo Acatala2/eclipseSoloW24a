2746d291631061a5ea774889d472a49ff52ddfa2
andre <andre@164.76.16.217>
2025-02-03 12:45:10 -0500

    Another push

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,7 +3,7 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main(String [] args) {
 		System.out.println("Howdy github Project");
-		System.out.println("This is my second print  Statement.");
+		System.out.println("This is my sec ond print  Statement.");
 		System.out.println("Programmer 2 did this.");
 		System.out.println("Programmer One did this.");
 
