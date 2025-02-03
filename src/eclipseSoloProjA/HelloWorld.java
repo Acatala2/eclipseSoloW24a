30b409d0a73f7f00a3ebfdf30704f419c6684d0f
andre <andre@164.76.16.217>
2025-02-03 12:00:48 -0500

    did some nifty stuff

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -1,5 +1,8 @@
 package eclipseSoloProjA;
 
 public class HelloWorld {
-
+	public static void main(String [] args) {
+		System.out.println("Howdy github Project");
+		System.out.println("This is my second println Statement");
+	}
 }
